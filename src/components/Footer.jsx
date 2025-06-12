2c960294d55aa475333b781ecb20eac7d894f297
Infinity2209 <anantanag91@gmail.com>
2025-06-12 14:51:12 +0530

    social media icon updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,13 +9,44 @@ const Footer = () => {
         alert('Chat functionality would be implemented here!');
     };
 
-    // Array of social media links with icons and URLs
+    // SVG icons for social media
+    const InstagramIcon = (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor" aria-hidden="true">
+            <g weight="regular"><path d="M128,80a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160ZM176,24H80A56.06,56.06,0,0,0,24,80v96a56.06,56.06,0,0,0,56,56h96a56.06,56.06,0,0,0,56-56V80A56.06,56.06,0,0,0,176,24Zm40,152a40,40,0,0,1-40,40H80a40,40,0,0,1-40-40V80A40,40,0,0,1,80,40h96a40,40,0,0,1,40,40ZM192,76a12,12,0,1,1-12-12A12,12,0,0,1,192,76Z"></path></g>
+        </svg>
+    );
+
+    const TikTokIcon = (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor" aria-hidden="true">
+            <g weight="regular"><path d="M224,72a48.05,48.05,0,0,1-48-48,8,8,0,0,0-8-8H128a8,8,0,0,0-8,8V156a20,20,0,1,1-28.57-18.08A8,8,0,0,0,96,130.69V88a8,8,0,0,0-9.4-7.88C50.91,86.48,24,119.1,24,156a76,76,0,0,0,152,0V116.29A103.25,103.25,0,0,0,224,128a8,8,0,0,0,8-8V80A8,8,0,0,0,224,72Zm-8,39.64a87.19,87.19,0,0,1-43.33-16.15A8,8,0,0,0,160,102v54a60,60,0,0,1-120,0c0-25.9,16.64-49.13,40-57.6v27.67A36,36,0,1,0,136,156V32h24.5A64.14,64.14,0,0,0,216,87.5Z"></path></g>
+        </svg>
+    );
+
+    const LinkedInIcon = (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor" aria-hidden="true">
+            <g weight="regular"><path d="M216,24H40A16,16,0,0,0,24,40V216a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V40A16,16,0,0,0,216,24Zm0,192H40V40H216V216ZM96,112v64a8,8,0,0,1-16,0V112a8,8,0,0,1,16,0Zm88,28v36a8,8,0,0,1-16,0V140a20,20,0,0,0-40,0v36a8,8,0,0,1-16,0V112a8,8,0,0,1,15.79-1.78A36,36,0,0,1,184,140ZM100,84A12,12,0,1,1,88,72,12,12,0,0,1,100,84Z"></path></g>
+        </svg>
+    );
+
+    const YouTubeIcon = (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor" aria-hidden="true">
+            <g weight="regular"><path d="M164.44,121.34l-48-32A8,8,0,0,0,104,96v64a8,8,0,0,0,12.44,6.66l48-32a8,8,0,0,0,0-13.32ZM120,145.05V111l25.58,17ZM234.33,69.52a24,24,0,0,0-14.49-16.4C185.56,39.88,131,40,128,40s-57.56-.12-91.84,13.12a24,24,0,0,0-14.49,16.4C19.08,79.5,16,97.74,16,128s3.08,48.5,5.67,58.48a24,24,0,0,0,14.49,16.41C69,215.56,120.4,216,127.34,216h1.32c6.94,0,58.37-.44,91.18-13.11a24,24,0,0,0,14.49-16.41c2.59-10,5.67-28.22,5.67-58.48S236.92,79.5,234.33,69.52Zm-15.49,113a8,8,0,0,1-4.77,5.49c-31.65,12.22-85.48,12-86,12H128c-.54,0-54.33.2-86-12a8,8,0,0,1-4.77-5.49C34.8,173.39,32,156.57,32,128s2.8-45.39,5.16-54.47A8,8,0,0,1,41.93,68c30.52-11.79,81.66-12,85.85-12h.27c.54,0,54.38-.18,86,12a8,8,0,0,1,4.77,5.49C221.2,82.61,224,99.43,224,128S221.2,173.39,218.84,182.47Z"></path></g>
+        </svg>
+    );
+
+    const TwitterIcon = (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor" aria-hidden="true">
+            <g weight="regular"><path d="M247.39,68.94A8,8,0,0,0,240,64H209.57A48.66,48.66,0,0,0,168.1,40a46.91,46.91,0,0,0-33.75,13.7A47.9,47.9,0,0,0,120,88v6.09C79.74,83.47,46.81,50.72,46.46,50.37a8,8,0,0,0-13.65,4.92c-4.31,47.79,9.57,79.77,22,98.18a110.93,110.93,0,0,0,21.88,24.2c-15.23,17.53-39.21,26.74-39.47,26.84a8,8,0,0,0-3.85,11.93c.75,1.12,3.75,5.05,11.08,8.72C53.51,229.7,65.48,232,80,232c70.67,0,129.72-54.42,135.75-124.44l29.91-29.9A8,8,0,0,0,247.39,68.94Zm-45,29.41a8,8,0,0,0-2.32,5.14C196,166.58,143.28,216,80,216c-10.56,0-18-1.4-23.22-3.08,11.51-6.25,27.56-17,37.88-32.48A8,8,0,0,0,92,169.08c-.47-.27-43.91-26.34-44-96,16,13,45.25,33.17,78.67,38.79A8,8,0,0,0,136,104V88a32,32,0,0,1,9.6-22.92A30.94,30.94,0,0,1,167.9,56c12.66.16,24.49,7.88,29.44,19.21A8,8,0,0,0,204.67,80h16Z"></path></g>
+        </svg>
+    );
+
+    // Array of social media links with inline SVG icons and URLs
     const socialLinks = [
-        { icon: 'https://cdn-icons-png.flaticon.com/512/174/174855.png', name: 'Instagram', url: '#' },
-        { icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968958.png', name: 'TikTok', url: '#' },
-        { icon: 'https://cdn-icons-png.flaticon.com/512/174/174857.png', name: 'LinkedIn', url: '#' },
-        { icon: 'https://cdn-icons-png.flaticon.com/512/1384/1384060.png', name: 'YouTube', url: '#' },
-        { icon: 'https://cdn-icons-png.flaticon.com/512/733/733585.png', name: 'Twitter', url: '#' }
+        { icon: InstagramIcon, name: 'Instagram', url: '#' },
+        { icon: TikTokIcon, name: 'TikTok', url: '#' },
+        { icon: LinkedInIcon, name: 'LinkedIn', url: '#' },
+        { icon: YouTubeIcon, name: 'YouTube', url: '#' },
+        { icon: TwitterIcon, name: 'Twitter', url: '#' }
     ];
 
     // Array of navigation links displayed in the footer
@@ -27,8 +58,8 @@ const Footer = () => {
     ];
 
     return (
-        <>
-            <div className="footer-gradient-bg"></div>
+        <div className='foot'>
+            <div className="new-radial-blur"></div>
             <footer className="footer">
                 <div className="footer-container">
                     <div className="footer-content">
@@ -45,7 +76,7 @@ const Footer = () => {
                                     target="_blank"
                                     rel="noopener noreferrer"
                                 >
-                                    <img src={social.icon} alt={social.name} />
+                                    {social.icon}
                                 </a>
                             ))}
                         </div>
@@ -71,13 +102,13 @@ const Footer = () => {
                 </div>
                 {/* Chat button */}
                 <div className="chat-button" onClick={openChat}>
-                    {/* <span className="chat-text">Chat now!</span> */}
+                    <p className="chat-text">Chat now!</p>
                     <div className="whatsapp-icon">
                         <img src="https://cdn-icons-png.flaticon.com/512/733/733585.png" alt="WhatsApp" />
                     </div>
                 </div>
             </footer>
-        </>
+        </div>
     );
 };
 
